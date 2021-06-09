1b8e586b544fd7cb96e90fc55930abf7c379b4a5
Nicholas Pym <nicholasp@bbd.co.za>
2021-06-09 21:25:52 +0200

    Added groups and schedules controllers

---
+++ Controllers/Groups.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ESPKnockOff.Models;
+
+namespace ESPKnockOff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Groups : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public Groups(ApplicationContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Controllers/Schedules.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ESPKnockOff.Models;
+
+namespace ESPKnockOff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Schedules : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public Schedules(ApplicationContext context)
+        {
+            _context = context;
+        }
+    }
+}
