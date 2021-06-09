73d1bd9b325b670488284e4c6a4b2da95a7719f4
Nicholas Pym <nicholasp@bbd.co.za>
2021-06-09 13:57:34 +0200

    Added provinces, municipalities and suburbs endpoints

---
+++ Controllers/Municipalities.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Municipalities : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public Municipalities(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Municipality> GetMunicipalities()
+        {
+            return _context.Municipality.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Municipality>> GetMunicipality(int id)
+        {
+            var province = await _context.Municipality.FindAsync(id);
+
+            if (province == null)
+            {
+                return NotFound();
+            }
+
+            return province;
+        }
+    }
+}
